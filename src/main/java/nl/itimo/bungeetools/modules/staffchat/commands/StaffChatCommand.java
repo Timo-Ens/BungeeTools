e70d553da0716e2f269cfd33c1735850ffcb4b1d
Timo-Ens <tomencherry10@gmail.com>
2021-03-31 15:36:00 +0200

    fix: staffchat tabcomplete returning error

--- src/main/java/nl/itimo/bungeetools/modules/staffchat/commands/StaffChatCommand.java
+++ src/main/java/nl/itimo/bungeetools/modules/staffchat/commands/StaffChatCommand.java
@@ -1,13 +1,19 @@
 package nl.itimo.bungeetools.modules.staffchat.commands;
 
+import com.google.common.collect.ImmutableSet;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 import nl.itimo.bungeetools.modules.staffchat.StaffChatModule;
 import nl.itimo.bungeetools.utils.Messages;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class StaffChatCommand extends Command implements TabExecutor {
 
@@ -82,6 +88,11 @@ public class StaffChatCommand extends Command implements TabExecutor {
 
     @Override
     public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
-        return null;
+        if (args.length != 1) return ImmutableSet.of();
+        Set<String> matches = new HashSet<>();
+        String search = args[0].toLowerCase();
+        String[] strings = new String[]{"toggle", "enable", "disable", "togglemute", "mute", "unmute"};
+        Arrays.stream(strings).filter(s -> s.startsWith(search)).forEach(matches::add);
+        return matches;
     }
 }
